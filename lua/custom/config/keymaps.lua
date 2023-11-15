450b4b959fcd5293cd1479d69bef4c5313b21f4f
Leo Capmartin <leo.capmartin@epita.fr>
2023-11-15 10:24:06 +0100

    add todo higlight

--- lua/custom/config/keymaps.lua
+++ lua/custom/config/keymaps.lua
@@ -1,52 +1,63 @@
 return {
-	global = function ()
-		-- disable leader
-		vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+  global = function()
+    -- disable leader
+    vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
-		-- Remap for dealing with word wrap
-		vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-		vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+    -- Remap for dealing with word wrap
+    vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+    vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
-		-- Diagnostic keymaps
-		vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-		vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-		vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-		vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-	end,
+    -- Diagnostic keymaps
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+  end,
 
-	nvterm = function ()
-		vim.keymap.set('n', '<A-h>', function () require('nvterm.terminal').toggle('horizontal') end)
-		vim.keymap.set('n', '<A-v>', function () require('nvterm.terminal').toggle('vertical') end)
-		vim.keymap.set('n', '<A-f>', function () require('nvterm.terminal').toggle('float') end)
-	end,
+  nvterm = function()
+    vim.keymap.set('n', '<A-h>', function()
+      require('nvterm.terminal').toggle 'horizontal'
+    end)
+    vim.keymap.set('n', '<A-v>', function()
+      require('nvterm.terminal').toggle 'vertical'
+    end)
+    vim.keymap.set('n', '<A-f>', function()
+      require('nvterm.terminal').toggle 'float'
+    end)
+  end,
 
-	telescope = function ()
-		pcall(require('telescope').load_extension, 'fzf')
-		vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-		vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-		vim.keymap.set('n', '<leader>s', function()
-			-- You can pass additional configuration to telescope to change theme, layout, etc.
-			require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-				winblend = 10,
-				previewer = false,
-			})
-		end, { desc = 'Fuzzily [s]earch in current buffer' })
+  telescope = function()
+    pcall(require('telescope').load_extension, 'fzf')
+    vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+    vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>s', function()
+      -- You can pass additional configuration to telescope to change theme, layout, etc.
+      require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+      })
+    end, { desc = 'Fuzzily [s]earch in current buffer' })
 
-		vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-		vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
-		vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
-		vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
-		vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
-		vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
-	end,
+    vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+    vim.keymap.set('n', '<leader>gs', require('telescope.builtin').git_status, { desc = '[G]it [S]tatus' })
+    vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
+    vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
+    vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
+    vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
+    vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
+  end,
 
-	nvimtree = function ()
-		vim.keymap.set('n', '<C-n>', '<CMD> NvimTreeToggle <CR>')
-		vim.keymap.set('n', '<C-f>', '<CMD> NvimTreeFocus <CR>')
-	end,
+  nvimtree = function()
+    vim.keymap.set('n', '<C-n>', '<CMD> NvimTreeToggle <CR>')
+    vim.keymap.set('n', '<C-f>', '<CMD> NvimTreeFocus <CR>')
+  end,
 
-	hover = function ()
-		vim.keymap.set('n', 'K', require("hover").hover, { desc = "hover.nvim"})
-		vim.keymap.set('n', '<C-k>', require("hover").hover_select, { desc = "hover.nvim (select)"})
-	end
+  hover = function()
+    vim.keymap.set('n', 'K', require('hover').hover, { desc = 'hover.nvim' })
+    vim.keymap.set('n', '<C-k>', require('hover').hover_select, { desc = 'hover.nvim (select)' })
+  end,
+
+  todo_comments = function()
+    vim.keymap.set('n', '<leader>ft', '<CMD> TodoTelescope <CR>', { desc = '[F]ind [T]odos' })
+  end,
 }
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,229 +4,238 @@
 -- See the kickstart.nvim README for more information
 
 local plugins = {
-	-- NOTE: First, some plugins that don't require any configuration
+  -- NOTE: First, some plugins that don't require any configuration
 
-	-- Git related plugins
-	'tpope/vim-fugitive',
-	'tpope/vim-rhubarb',
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
 
-	-- Detect tabstop and shiftwidth automatically
-	'tpope/vim-sleuth',
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
 
-	-- NOTE: This is where your plugins related to LSP can be installed.
-	--  The configuration is done below. Search for lspconfig to find it below.
-	{
-		-- LSP Configuration & Plugins
-		'neovim/nvim-lspconfig',
-		dependencies = {
-			-- Automatically install LSPs to stdpath for neovim
-			{ 'williamboman/mason.nvim', config = true },
-			'williamboman/mason-lspconfig.nvim',
+  -- NOTE: This is where your plugins related to LSP can be installed.
+  --  The configuration is done below. Search for lspconfig to find it below.
+  {
+    -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      { 'williamboman/mason.nvim', config = true },
+      'williamboman/mason-lspconfig.nvim',
 
-			-- Useful status updates for LSP
-			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-			{ 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+      -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
 
-			-- Additional lua configuration, makes nvim stuff amazing!
-			'folke/neodev.nvim',
-		},
-	},
+      -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim',
+    },
+  },
 
-	{
-		-- formating
-		'mhartington/formatter.nvim',
-		opts = function ()
-			return require("custom.plugins.formatter")
-		end,
-		config = function (_, opts)
-			require("formatter").setup(opts)
-		end
-	},
+  {
+    -- formating
+    'mhartington/formatter.nvim',
+    opts = function()
+      return require 'custom.plugins.formatter'
+    end,
+    config = function(_, opts)
+      require('formatter').setup(opts)
+    end,
+  },
 
-	{
-		-- Autocompletion
-		'hrsh7th/nvim-cmp',
-		dependencies = {
-			-- Snippet Engine & its associated nvim-cmp source
-			'L3MON4D3/LuaSnip',
-			'saadparwaiz1/cmp_luasnip',
+  {
+    -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
 
-			-- Adds LSP completion capabilities
-			'hrsh7th/cmp-nvim-lsp',
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
 
-			-- Adds a number of user-friendly snippets
-			'rafamadriz/friendly-snippets',
-		},
-		opts = function ()
-			return require('custom.plugins.cmp')
-		end,
-		config = function (_, opts)
-			require('cmp').setup(opts)
-		end
-	},
+      -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
+    },
+    opts = function()
+      return require 'custom.plugins.cmp'
+    end,
+    config = function(_, opts)
+      require('cmp').setup(opts)
+    end,
+  },
 
-	-- Useful plugin to show you pending keybinds.
-	{ 'folke/which-key.nvim', opts = {} },
-	{
-		-- Adds git related signs to the gutter, as well as utilities for managing changes
-		'lewis6991/gitsigns.nvim',
-		opts = {
-			-- See `:help gitsigns.txt`
-			signs = {
-				add = { text = '+' },
-				change = { text = '~' },
-				delete = { text = '_' },
-				topdelete = { text = '‾' },
-				changedelete = { text = '~' },
-			},
-			on_attach = function(bufnr)
-				vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-				vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-				vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
-			end,
-		},
-	},
+  -- Useful plugin to show you pending keybinds.
+  { 'folke/which-key.nvim', opts = {} },
+  {
+    -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      -- See `:help gitsigns.txt`
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      on_attach = function(bufnr)
+        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
+        vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
+        vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+      end,
+    },
+  },
 
-	{
-		"olimorris/onedarkpro.nvim",
-		priority = 1000, -- Ensure it loads first
-		config = function()
-			require("onedarkpro").setup({
-				styles = {
-					keywords = "italic",
-					conditionals = "italic",
-					comments = "italic",
-					types = "italic",
-				}
-			})
-			vim.cmd.colorscheme 'onedark'
-		end,
-	},
+  {
+    'olimorris/onedarkpro.nvim',
+    priority = 1000, -- Ensure it loads first
+    config = function()
+      require('onedarkpro').setup {
+        styles = {
+          keywords = 'italic',
+          conditionals = 'italic',
+          comments = 'italic',
+          types = 'italic',
+        },
+      }
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
 
-	{
-		-- Set lualine as statusline
-		'nvim-lualine/lualine.nvim',
-		-- See `:help lualine.txt`
-		opts = {
-			options = {
-				theme = 'onedark',
-				-- component_separators = '|',
-				component_separators = { left = '', right = ''},
-				section_separators = { left = '', right = ''},
-				-- section_separators = '',
-			},
-		},
-	},
+  {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        theme = 'onedark',
+        -- component_separators = '|',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        -- section_separators = '',
+      },
+    },
+  },
 
-	{
-		-- Add indentation guides even on blank lines
-		'lukas-reineke/indent-blankline.nvim',
-		-- Enable `lukas-reineke/indent-blankline.nvim`
-		-- See `:help indent_blankline.txt`
-		main = "ibl",
-		opts = {},
-	},
+  {
+    -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help indent_blankline.txt`
+    main = 'ibl',
+    opts = {},
+  },
 
-	-- "gc" to comment visual regions/lines
-	{ 'numToStr/Comment.nvim',
-		opts = function ()
-			return require('custom.plugins.Comment')
-		end,
-		config = function (_, opts)
-			require('Comment').setup(opts)
-		end
-	},
+  -- "gc" to comment visual regions/lines
+  {
+    'numToStr/Comment.nvim',
+    opts = function()
+      return require 'custom.plugins.Comment'
+    end,
+    config = function(_, opts)
+      require('Comment').setup(opts)
+    end,
+  },
 
-	-- Fuzzy Finder (files, lsp, etc)
-	{
-		'nvim-telescope/telescope.nvim',
-		branch = '0.1.x',
-		dependencies = {
-			'nvim-lua/plenary.nvim',
-			-- Fuzzy Finder Algorithm which requires local dependencies to be built.
-			-- Only load if `make` is available. Make sure you have the system
-			-- requirements installed.
-			{
-				'nvim-telescope/telescope-fzf-native.nvim',
-				-- NOTE: If you are having trouble with this installation,
-				--       refer to the README for telescope-fzf-native for more instructions.
-				build = 'make',
-				cond = function()
-					return vim.fn.executable 'make' == 1
-				end,
-			},
-		},
-		opts = function ()
-			return require('custom.plugins.telescope')
-		end,
-		config = function (_, opts)
-			require('telescope').setup(opts)
-			-- pcall(require('telescope').load_extension, 'fzf')
-			require('custom.config.keymaps').telescope()
-		end
-	},
+  -- Fuzzy Finder (files, lsp, etc)
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+      -- Only load if `make` is available. Make sure you have the system
+      -- requirements installed.
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+    },
+    opts = function()
+      return require 'custom.plugins.telescope'
+    end,
+    config = function(_, opts)
+      require('telescope').setup(opts)
+      -- pcall(require('telescope').load_extension, 'fzf')
+      require('custom.config.keymaps').telescope()
+    end,
+  },
 
-	{
-		-- Highlight, edit, and navigate code
-		'nvim-treesitter/nvim-treesitter',
-		dependencies = {
-			'nvim-treesitter/nvim-treesitter-textobjects',
-		},
-		build = ':TSUpdate',
-		opts = function ()
-			return require('custom.plugins.treesitter')
-		end,
-		config = function (_, opts)
-			require('nvim-treesitter.configs').setup(opts)
-		end
-	},
+  {
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+    opts = function()
+      return require 'custom.plugins.treesitter'
+    end,
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+    end,
+  },
 
-	{
-		'windwp/nvim-autopairs',
-		event = "InsertEnter",
-		opts = {} -- this is equalent to setup({}) function
-	},
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    opts = {}, -- this is equalent to setup({}) function
+  },
 
-	{
-		'NvChad/nvterm',
-		config = function ()
-			require("nvterm").setup()
-			require("custom.config.keymaps").nvterm()
-		end,
-	},
+  {
+    'NvChad/nvterm',
+    config = function()
+      require('nvterm').setup()
+      require('custom.config.keymaps').nvterm()
+    end,
+  },
 
-	{
-		'nvim-tree/nvim-tree.lua',
-		dependencies = {
-			'nvim-tree/nvim-web-devicons'
-		},
-		opts = function ()
-			return require('custom.plugins.nvim-tree')
-		end,
-		config = function (_, opts)
-			require('nvim-tree').setup(opts)
-			require("custom.config.keymaps").nvimtree()
-		end
-	},
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    opts = function()
+      return require 'custom.plugins.nvim-tree'
+    end,
+    config = function(_, opts)
+      require('nvim-tree').setup(opts)
+      require('custom.config.keymaps').nvimtree()
+    end,
+  },
 
-	{
-		'norcalli/nvim-colorizer.lua',
-		config = function ()
-			require("colorizer").setup()
-		end
-	},
+  {
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup()
+    end,
+  },
 
-	{
-		'lewis6991/hover.nvim',
-		opts = function ()
-			return require("custom.plugins.hover")
-		end,
-		config = function (_, opts)
-			require("hover").setup(opts)
-			require("custom.config.keymaps").hover()
-		end
-	}
+  {
+    'lewis6991/hover.nvim',
+    opts = function()
+      return require 'custom.plugins.hover'
+    end,
+    config = function(_, opts)
+      require('hover').setup(opts)
+      require('custom.config.keymaps').hover()
+    end,
+  },
 
+  {
+    'folke/todo-comments.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('todo-comments').setup()
+      require('custom.config.keymaps').todo_comments()
+    end,
+  },
 }
 
 return plugins
